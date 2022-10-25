564289ab3d7c14a53f8677003746c5509c1d99ee
AnnoyingDoge <60070276+AnnoyingDoge@users.noreply.github.com>
2022-10-25 12:53:58 -0400

    small edits

--- portfolio-minimal/src/pages/index.js
+++ portfolio-minimal/src/pages/index.js
@@ -16,9 +16,9 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Me" />
-        <InterestsSection sectionId="details" heading="Details" />
+        <InterestsSection sectionId="skills" heading="Skills" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
         <ContactSection sectionId="github" heading="Contact" />
       </Page>
