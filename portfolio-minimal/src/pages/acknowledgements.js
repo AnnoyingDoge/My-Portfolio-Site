e1b86aacdc7e08fee6dbcfddf5465d75ddcf28a4
AnnoyingDoge <60070276+AnnoyingDoge@users.noreply.github.com>
2022-10-25 21:48:47 -0400

    Acknowledgements page

--- portfolio-minimal/src/pages/acknowledgements.js
+++ portfolio-minimal/src/pages/acknowledgements.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { LegalSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
+import { LegalSection, Page, Section, Animation } from 'gatsby-theme-portfolio-minimal';
 
 export default function AcknowledgementsPage() {
-  return (
-    <>
-      <Seo title="Acknowledgements" useTitleTemplate={true} noIndex={true} />
-      <Page>
-        
-      </Page>
-    </>
-  );
+    return (
+        <Page>
+            <Animation type="fadeUp">
+                <Section heading="Acknowledgements">
+                <LegalSection sectionId="acknowledgements" heading="" />
+                </Section>
+            </Animation>
+        </Page>
+    );
 }
+
+// export default function AcknowledgementsPage() {
+//     return (
+//         <Page>
+//             <Animation type="fadeUp">
+//                 <Section heading="Acknowledgements">
+//                     <p>
+//                         This site is built using a template by <a href="https://github.com/konstantinmuenster/" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>Konstantin Münster.</u></a>
+//                     </p>
+//                     <p>
+//                         Check out their <a href="https://github.com/konstantinmuenster/gatsby-theme-portfolio-minimal/" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>Github Repository!</u></a>
+//                     </p>
+                    
+//                 </Section>
+//             </Animation>
+//         </Page>
+//     );
+// }
